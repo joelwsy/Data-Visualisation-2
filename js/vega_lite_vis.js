7b1f6afa70466a9a7cc6da4e3a9689062e8427c1
Joel Wong <joelwsy236@gmail.com>
2026-05-22 00:48:19 +0800

    Initial Commit of all 11 charts

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,37 @@
+// Chapter 1
+var vg_1 = "charts/01_nesting_map.json";
+vegaEmbed("#nesting_map", vg_1).catch(console.error);
+
+var vg_2 = "charts/02_species_region_heatmap.json";
+vegaEmbed("#species_heatmap", vg_2).catch(console.error);
+
+// Chapter 2
+var vg_3 = "charts/03_leatherback_collapse.json";
+vegaEmbed("#leatherback_collapse", vg_3).catch(console.error);
+
+var vg_4 = "charts/04_indexed_species_trajectories.json";
+vegaEmbed("#species_trajectories", vg_4).catch(console.error);
+
+// Chapter 3
+var vg_5 = "charts/05_threats_lollipop.json";
+vegaEmbed("#threats_lollipop", vg_5).catch(console.error);
+
+var vg_6 = "charts/06_egg_journey_sankey.json";
+vegaEmbed("#egg_journey", vg_6).catch(console.error);
+
+// Chapter 4
+var vg_7 = "charts/07_isotype_sex_ratio.json";
+vegaEmbed("#sex_ratio", vg_7).catch(console.error);
+
+var vg_8 = "charts/08_connected_scatter_sst.json";
+vegaEmbed("#sst_scatter", vg_8).catch(console.error);
+
+// Chapter 5
+var vg_9 = "charts/09_diverging_hatching.json";
+vegaEmbed("#hatching_bars", vg_9).catch(console.error);
+
+var vg_10 = "charts/10_tourists_vs_nests.json";
+vegaEmbed("#tourists_nests", vg_10).catch(console.error);
+
+var vg_11 = "charts/11_calendar_heatmap.json";
+vegaEmbed("#calendar_heatmap", vg_11).catch(console.error);
