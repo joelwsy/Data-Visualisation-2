8622f74ae535f2dae61a14c1ec53c7b3663113b1
joelwsy <joelwsy236@gmail.com>
2026-05-31 17:11:38 +0800

    vegalite changes to show index

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,6 @@
 // Idiom 1: Geospatial Proportional Symbol Map
 var vg_1 = "charts/01_proportional_map.json";
-vegaEmbed("#proportional_map", vg_1).catch(console.error);
+vegaEmbed("#geo_map", vg_1).catch(console.error);
 
 // Idiom 2: Diverging Bar + Nest Failure Line (vconcat)
 var vg_2 = "charts/02_decline_lines.json";
@@ -8,7 +8,7 @@ vegaEmbed("#decline_lines", vg_2).catch(console.error);
 
 // Idiom 3: Isotype Array of Seized Commodities
 var vg_3 = "charts/03_sst_sex_failure.json";
-vegaEmbed("#sst_sex_failure", vg_3).catch(console.error);
+vegaEmbed("#sst_failures", vg_3).catch(console.error);
 
 // Idiom 4: Dual-Axis Area + Line (Landings vs Seizures)
 var vg_4 = "charts/04_landings_vs_seizures.json";
